ac09fd25557196cb45463393ea55ae88a5b5ac33
Lihoubrak <blh.braklihou18@gmail.comgit config --global user.email blh.braklihou18@gmail.comgit config --global user.email blh.braklihou18@gmail.com>
2023-07-31 20:03:40 +0700

    change url

--- requestMethod.js
+++ requestMethod.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 import Cookies from "js-cookie";
-const BASE_URL = "http://localhost:3000/api/";
-const TOKEN = Cookies.get("userInfo")
+const BASE_URL = "https://expressjsshopping-kh3k.onrender.com/api/";
+const TOKEN = Cookies.get("userInfo");
 export const publicRequest = axios.create({
-    baseURL: BASE_URL,
-  });
+  baseURL: BASE_URL,
+});
 
 export const TokenRequest = axios.create({
-  baseURL : BASE_URL,
-  headers : { token: `Bearer ${TOKEN}` }
-})
-
+  baseURL: BASE_URL,
+  headers: { token: `Bearer ${TOKEN}` },
+});
